d7692466211a5606fcd4a46e30a08287cc6c89ad
Mayank Yadav <mayankyadav@Mayanks-MacBook-Air.local>
2025-12-28 13:09:56 +0530

    Made the server RESP compatible. Redis server completed.

--- src/test/java/com/miniredis/network/NetworkIntegrationTest.java
+++ src/test/java/com/miniredis/network/NetworkIntegrationTest.java
@@ -7,56 +7,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class NetworkIntegrationTest {
     
-    // @Test
-    // void testBasicPingPong() throws IOException{
-    //     // Start server in a background thread so that it does not black the test
-    //     int port = 9001;
-    //     new Thread(() -> {
-    //         RedisServer server = new RedisServer(port);
-    //         server.start();
-    //     }).start();
-
-    //     // giving the server time for starting
-    //     try {Thread.sleep(500); } catch (InterruptedException e){}
-
-    //     // connect as a client
-    //     try(Socket socket = new Socket("localhost", port);
-    //         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-    //         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))){
-    //         out.println("PING");
-    //         String response = in.readLine();
-    //         assertEquals("+PONG", response);
-    //     }
-    // }
-
     @Test
-    void testGarbage() throws IOException {
-        // ... setup server on port 9002 (Use a NEW port to avoid ghosts) ...
-        int port = 9002;
+    void testBasicPingPong() throws IOException{
+        // Start server in a background thread so that it does not black the test
+        int port = 9001;
         new Thread(() -> {
             RedisServer server = new RedisServer(port);
             server.start();
         }).start();
 
-        // Give it a moment to start
-        try { Thread.sleep(500); } catch (InterruptedException e) {}
+        // giving the server time for starting
+        try {Thread.sleep(500); } catch (InterruptedException e){}
 
-        try (Socket socket = new Socket("localhost", port);
+        // connect as a client
+        try(Socket socket = new Socket("localhost", port);
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))){
+
+            out.println("*1\r\n$4\r\nPING\r\n");
+            out.flush();
             
-            // Send absolute nonsense
-            out.println("GARBAGE_DATA");
-            
-            // If the server is using RespParser, it should throw an error and close the connection.
-            // So 'response' should be null.
             String response = in.readLine();
-            
-            System.out.println("Server responded with: " + response);
-            
-            // If this PASSES (response is null), your Parser is working.
-            // If this FAILS (response is -ERR unknown command), your Parser is NOT connected.
-            assertNull(response, "Server should have closed connection on bad protocol");
+            assertEquals("+PONG", response);
         }
     }
 }
